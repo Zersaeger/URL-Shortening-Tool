68dc2bd73b10594d2896307355def3bc70bfa32b
Zersaeger <Zersaeger@users.noreply.github.com>
2023-06-09 20:53:32 +0200

    Added function

--- index.js
+++ index.js
@@ -19,11 +19,18 @@ app.post('/', async (req, res) => {
     const data = req.body;
     const newurl = Math.floor(Math.random() * 1000000);
     await db.run('INSERT INTO URLs (origin, new) VALUES (?,?)', data.url, newurl);
-    res.render('index', { message: "Here is your new URL: " + newurl.toString() })
+    res.render('index', { message: "Here is your new URL: " + newurl.toString() });
 });
 
 app.get('/:url', async (req, res) => {
     const data = await db.get('SELECT origin FROM URLs WHERE new = ?', req.params.url);
-    res.redirect("http://" + data.origin);
+    res.redirect(addhttp(data));
 });
+
+function addhttp(data) {
+    if (!data.origin.startsWith('https://')) {
+        return "https://" + data.origin;
+    }
+    return data.origin;
+}
 app.listen('3000');
